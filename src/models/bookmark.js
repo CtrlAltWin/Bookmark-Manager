68d58f246392fd36d2d8a39e608b111a631873b0
Raunak <raunakkumar7245@gmail.com>
2025-05-27 13:55:07 +0530

    fix: updated bookmark model and provided clear messages

--- src/models/bookmark.js
+++ src/models/bookmark.js
@@ -5,8 +5,9 @@ const bookmarkSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-      validator: (title) => title.length <= 20,
-      message: () => "Maximum length of title can be 20",
+      validator: (title) => title.length >= 1 && title.length <= 20,
+      message: () =>
+        "Title is required and must be between 1 and 20 characters.",
     },
   },
   url: {
@@ -16,34 +17,39 @@ const bookmarkSchema = new mongoose.Schema({
       validator: (url) => {
         return /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?$/.test(url);
       },
-      message: () => `Url is not a valid URL`,
+      message: () =>
+        "Please provide a valid URL (starting with http:// or https://).",
     },
   },
   description: {
     type: String,
     validate: {
-      validator: (description) => description.length <= 50,
-      message: () => "Maximum length of description can be 50",
+      validator: (description) => !description || description.length <= 50,
+      message: () => "Description cannot exceed 50 characters.",
     },
   },
   tags: {
     type: [String],
+    required: true,
     validate: {
       validator: (tags) =>
-        tags.every((tag) => tag.length <= 20) && tags.length <= 5,
+        tags.length >= 1 &&
+        tags.length <= 5 &&
+        tags.every((tag) => tag.length >= 1 && tag.length <= 20),
       message: () =>
-        "Maximum number of tags can be 5 and length of each tag can be 20",
+        "You must provide between 1 and 5 tags, each between 1 and 20 characters.",
     },
   },
   category: {
     type: String,
     validate: {
-      validator: (value) => value.length <= 20,
-      message: () => "Maximum length of category can be 20",
+      validator: (category) => !category || category.length <= 20,
+      message: () => "Category cannot exceed 20 characters.",
     },
   },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
+    required: true,
     ref: "User",
   },
   createdAt: {
